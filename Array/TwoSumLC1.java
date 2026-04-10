53d9dd0b7f3c862b4d5976d76d4787d27497cc7a
Ahmad, Sameer <Sameer.Ahmad@ncrvoyix.com>
2026-04-10 21:00:08 +0530

    Initial Shagun

---
+++ Array/TwoSumLC1.java
@@ -0,0 +1,34 @@
+//LC1 TwoSum 
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumLC1 {
+        public int[] twoSum(int[] nums,int target){
+
+Map<Integer, Integer> idx = new HashMap<>(nums.length * 2);
+    for (int i = 0; i < nums.length; i++) {
+        int need = target - nums[i];
+        if (idx.containsKey(need)) {
+            return new int[]{idx.get(need), i};
+        }
+        idx.put(nums[i], i);
+    }
+    return new int[]{-1, -1}; // if not guaranteed
+
+    }
+
+    public int[] twoSumMW(int[] nums, int target) {
+    if (nums == null || nums.length < 2) return new int[]{-1, -1};
+
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                return new int[]{i, j};
+            }
+        }
+    }
+    return new int[]{-1, -1};
+}
+}
